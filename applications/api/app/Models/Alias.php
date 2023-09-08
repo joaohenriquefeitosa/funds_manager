c83e0df16233786b0a9ee5cf44e5b88bc990e50e
Joao Henrique <joaohenriquefsf@gmail.com>
2023-09-07 22:33:56 -0300

    adding models and migrations

---
+++ applications/api/app/Models/Alias.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alias extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'fund_id', 
+        'alias'
+    ];
+
+    public function fund()
+    {
+        return $this->belongsTo(Fund::class);
+    }
+
+}
